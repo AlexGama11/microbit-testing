f4400920376d87bf10262c169f7a95ced921de60
Alex Gama <alexgama1612@gmail.com>
2022-10-24 11:37:34 +0100

    More microbit stuff

--- main.ts
+++ main.ts
@@ -7,14 +7,19 @@ function Shapes () {
     basic.showIcon(IconNames.LeftTriangle)
     basic.showIcon(IconNames.Pitchfork)
 }
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
 input.onButtonPressed(Button.B, function () {
     Shapes()
 })
 function Radio () {
-    radio.setGroup(1)
     radio.sendString("Hello World!")
+    basic.showString("Message Sent!")
 }
+radio.setGroup(1)
 basic.showString("A:Radio, B:Shapes")
+let Number2 = 0
 basic.forever(function () {
-	
+    Number2 += 1
 })
