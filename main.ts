07c312ff3c1733876433fe6ed0d0368deef2450f
Alex Gama <alexgama1612@gmail.com>
2022-10-24 11:26:38 +0100

    First code

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    Radio()
+})
+function Shapes () {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Tortoise)
+    basic.showIcon(IconNames.LeftTriangle)
+    basic.showIcon(IconNames.Pitchfork)
+}
+input.onButtonPressed(Button.B, function () {
+    Shapes()
+})
+function Radio () {
+    radio.setGroup(1)
+    radio.sendString("Hello World!")
+}
+basic.showString("A:Radio, B:Shapes")
 basic.forever(function () {
 	
 })
