a75bd8c6b18fbe3087f9fb1159c3ab9429b3ed49
Alex Gama <alexgama1612@gmail.com>
2022-10-24 12:04:36 +0100

    moew features

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.clearScreen()
+    basic.showNumber(receivedNumber)
+})
 input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
     Radio()
+    Number2 = 0
+    Bluetooth = true
 })
 function Shapes () {
     basic.showIcon(IconNames.Heart)
@@ -8,18 +15,33 @@ function Shapes () {
     basic.showIcon(IconNames.Pitchfork)
 }
 radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
     basic.showString(receivedString)
 })
 input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
     Shapes()
+    Number2 = 0
+    Bluetooth = false
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    basic.showNumber(randint(0, 100))
+    basic.pause(2000)
+    Number2 = 0
 })
 function Radio () {
     radio.sendString("Hello World!")
     basic.showString("Message Sent!")
 }
+let Bluetooth = false
+let Number2 = 0
 radio.setGroup(1)
 basic.showString("A:Radio, B:Shapes")
-let Number2 = 0
+Number2 = 0
 basic.forever(function () {
-    Number2 += 1
+    if (!(Bluetooth)) {
+        Number2 += 1
+        basic.showNumber(Number2)
+    }
 })
